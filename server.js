7ba31bede146c6d300624dd067b35d0cb00cdcb0
Kirabo Jelly <kirabojellyrollings043@gmail.com>
2026-05-03 15:20:36 +0300

    fix server to use env variables

--- server.js
+++ server.js
@@ -1,7 +1,3 @@
-process.env.DATABASE_URL = "postgresql://postgres:postgres123@localhost:5432/finance_tracker";
-process.env.JWT_SECRET = "your_super_secret_key_change_this";
-process.env.PORT = "3000";
-
 const app = require('./src/app');
 
 const PORT = process.env.PORT || 3000;
